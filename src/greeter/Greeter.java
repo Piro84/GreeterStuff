c00c4e0c82b95db0fd61d539a680f30e66910128
Nirbhay Vig <19vign@elmbrookstudents.org>
2018-01-30 10:35:28 -0600

    Modified Greeter Class
    Created:
    Name and age fields
    Defult and alternate constructor
    and toString

--- src/greeter/Greeter.java
+++ src/greeter/Greeter.java
@@ -1,5 +1,22 @@
 package greeter;
 
 public class Greeter {
-
+	
+	private int age;
+	private String name;
+	
+	public Greeter() {
+		age = 0;
+		name = "";
+	}
+	
+	public Greeter(int ageX, String nameX) {
+		age = ageX;
+		name = nameX;
+	}
+	
+	public String toString() {
+		String temp = ("Greeter: age: " + age + ", name: " + name);
+		return temp;
+	}
 }
