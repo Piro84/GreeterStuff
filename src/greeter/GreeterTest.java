4e73469abab6e61da1335ed9c8b32f6880ba139d
Nirbhay Vig <19vign@elmbrookstudents.org>
2018-01-30 10:45:00 -0600

    Created A 1-D Array of Greeter Objects
    Created array and displayed state of each object.

--- src/greeter/GreeterTest.java
+++ src/greeter/GreeterTest.java
@@ -7,6 +7,18 @@ public class GreeterTest {
 		
 		Greeter one = new Greeter(10, "Joe");
 		System.out.println(one.toString());
+		
+		Greeter[] greets = new Greeter[5];
+		
+		greets[0] = new Greeter(11, "Bob");
+		greets[1] = new Greeter(12, "Billy");
+		greets[2] = new Greeter(13, "Jacob");
+		greets[3] = new Greeter(14, "Smith");
+		greets[4] = new Greeter(15, "Paul");
+		
+		for(int i = 0; i < greets.length ; i++) {
+			System.out.println(greets[i].toString());
+		}
 	}
 
 }
